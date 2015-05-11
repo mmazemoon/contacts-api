87e81e5e5b36f9a9f6e7d3e7891d90ef9ab0b3fa
Mark Azemoon <mmazemoon@gmail.com>
2015-05-11 16:22:39 -0700

    Add associations between User and Contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,13 @@
 class Contact < ActiveRecord::Base
   validates :name, :email, :user_id, presence: true
   validates :email, uniqueness: { scope: :user_id }
+
+  belongs_to(
+    :user,
+    class_name: 'User',
+    foreign_key: :user_id,
+    primary_key: :id
+  )
+
+
 end
