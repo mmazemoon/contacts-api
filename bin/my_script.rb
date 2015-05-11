1ca4a410fcde4644c293bfd667c49736601415bf
Mark Azemoon <mmazemoon@gmail.com>
2015-05-11 14:22:27 -0700

    Add create and show actions to Users rendering JSON

--- bin/my_script.rb
+++ bin/my_script.rb
@@ -5,7 +5,15 @@ url = Addressable::URI.new(
   scheme: 'http',
   host: 'localhost',
   port: 3000,
-  path: '/users.html'
+  path: '/users/123',
+  query_values: {
+    'top_level' => 'top_level_val',
+    'some_category[a_key]' => 'another value',
+    'some_category[a_second_key]' => 'yet another value',
+    'some_category[inner_inner_hash][key]' => 'value',
+    'some_category[inner_inner_hash][key2]' => 'value2',
+    'something_else' => 'aaahhhhh'
+  }
 ).to_s
 
-puts RestClient.get(url)
+puts RestClient.get(url, :param1 => 'one' )
