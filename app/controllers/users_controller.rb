270dacc4ae7710764b419453e3f2b2099ce30fd0
Mark Azemoon <mmazemoon@gmail.com>
2015-05-11 15:14:47 -0700

    Complete all actions (add show, update, destroy)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,40 @@ class UsersController < ApplicationController
   end
 
   def create
-    render json: "I'm in the create action!".to_json
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
   def show
-    render json: params.to_json
+    user = User.find(params[:id])
+    render json: user
   end
+
+  def update
+    user = User.find(params[:id])
+    user_params.each do |key, val|
+      user.send("#{key}=", val)
+    end
+    user.save!
+    render json: user
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    render json: true
+  end
+
+
+private
+
+  def user_params
+    params[:user].permit(:name, :email)
+  end
+
 
 end
--- bin/my_script.rb
+++ bin/my_script.rb
@@ -1,19 +1,50 @@
 require 'addressable/uri'
 require 'rest-client'
 
-url = Addressable::URI.new(
-  scheme: 'http',
-  host: 'localhost',
-  port: 3000,
-  path: '/users/123',
-  query_values: {
-    'top_level' => 'top_level_val',
-    'some_category[a_key]' => 'another value',
-    'some_category[a_second_key]' => 'yet another value',
-    'some_category[inner_inner_hash][key]' => 'value',
-    'some_category[inner_inner_hash][key2]' => 'value2',
-    'something_else' => 'aaahhhhh'
-  }
-).to_s
+def create_user
+  url = Addressable::URI.new(
+    scheme: 'http',
+    host: 'localhost',
+    port: 3000,
+    path: '/users.json',
+  ).to_s
+  begin
+    puts RestClient.post(url, :user => {
+    name: "Tiger"
+    } )
+  rescue RestClient::Exception => e
+    puts e.message
+  end
+end
 
-puts RestClient.get(url, :param1 => 'one' )
+def update_user
+  url = Addressable::URI.new(
+    scheme: 'http',
+    host: 'localhost',
+    port: 3000,
+    path: '/users/4.json',
+  ).to_s
+  begin
+    puts RestClient.patch(url, :user => {
+    email: "tiger@example.com"
+    } )
+  rescue RestClient::Exception => e
+    puts e.message
+  end
+end
+
+def destroy_user
+  url = Addressable::URI.new(
+    scheme: 'http',
+    host: 'localhost',
+    port: 3000,
+    path: '/users/3.json',
+  ).to_s
+  begin
+    puts RestClient.delete(url)
+  rescue RestClient::Exception => e
+    puts e.message
+  end
+end
+
+destroy_user
