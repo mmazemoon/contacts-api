934feb0139bdb1c1dbfd71c1f9d7b242082fbb4a
Mark Azemoon <mmazemoon@gmail.com>
2015-05-11 17:40:36 -0700

    Add ContactShares controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :update, :destroy, :create]
   # we don't want edit and new because those are for forms
   resources :contacts, only: [:index, :show, :update, :destroy, :create]
+
+  resources :contact_shares, only: [:destroy, :create]
 end
